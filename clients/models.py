c3b1a3af75c8090e3a802157842b802356da92dc
David <davidmejia.05@hotmail.com>
2022-07-13 22:19:55 -0500

    services app

--- clients/models.py
+++ clients/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
-class Client(models.Model):
-    name = models.CharField(max_length=20)
+class Person(models.Model):
+    first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     second_last_name = models.CharField(max_length=20)
 
+    class Meta:
+        abstract = True
+
     def __str__(self):
-        return '{} {} {}'.format(self.name, self.last_name, self.second_last_name)
+        return '{} {} {}'.format(self.first_name, self.last_name, self.second_last_name)
+        
+class Client(Person):
+    phone_number = models.CharField(max_length=10, null=True, blank=True) 
+
