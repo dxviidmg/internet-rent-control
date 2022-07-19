383b24a9365a1a9437d3b4e30c764adc6a63d1fa
David <davidmejia.05@hotmail.com>
2022-07-19 09:49:17 -0500

    delete heroku.py

--- internet_rent_control/wsgi.py
+++ internet_rent_control/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internet_rent_control.heroku')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'internet_rent_control.settings')
 
 application = get_wsgi_application()
