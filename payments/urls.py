ccb8d86fd2aebf09dd90be932eb85048dbb2f956
David <davidmejia.05@hotmail.com>
2022-07-27 11:27:52 -0500

    borrar pago

--- payments/urls.py
+++ payments/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PaymentListView, PaymentCreateView, PaymentsSumByMonthListView
+from .  import views
 
 
 app_name = 'payments'
 
 urlpatterns = [
-    path('payment-list/', PaymentListView.as_view(), name='payment-list'),
-    path('payment-create/<int:pk>/', PaymentCreateView.as_view(), name='payment-create'),
-    path('payments-sum-by-month/', PaymentsSumByMonthListView.as_view(), name='payments-sum-by-month-list'),
+    path('payment-list/', views.PaymentListView.as_view(), name='payment-list'),
+    path('payment-create/<int:pk>/', views.PaymentCreateView.as_view(), name='payment-create'),
+    path('payment-delete/<int:pk>/', views.PaymentDeleteView.as_view(), name='payment-delete'),
+    path('payments-sum-by-month/', views.PaymentsSumByMonthListView.as_view(), name='payments-sum-by-month-list'),
 ]
